8a3a08e7060ee1e672b7f142aa3df276fa127321
Paweł Płatek <e2.8a.95@gmail.com>
2020-06-19 11:34:55 +0200

    highlight each word

--- rozpoznawaczek/docx_highlight.py
+++ rozpoznawaczek/docx_highlight.py
@@ -1,5 +1,6 @@
 # !/usr/bin/env python
 # -*- coding: utf-8 -*-
+
 """Tool for highlighting diminutives in a docx file.
     Example:
         
@@ -8,42 +9,99 @@
     * Dominik Sepioło
     * Paweł Płatek
 """
-
+# fmt: on
 
 import argparse
+from copy import copy
 from os.path import isfile
+from sys import exit
 
 from docx import Document  # type: ignore
+from docx.dml.color import ColorFormat  # type: ignore
+from docx.enum.dml import MSO_COLOR_TYPE  # type: ignore
 from docx.enum.text import WD_COLOR_INDEX  # type: ignore
 from docx.exceptions import PythonDocxError  # type: ignore
+from docx.text.font import Font  # type: ignore
+from docx.text.run import Run  # type: ignore
 
 from rozpoznawaczek import L, find_diminutives
 
 
-def highlight(document):
+def copy_style(new_element, original_element):
+    if type(new_element) != type(original_element):
+        raise Exception('Types of objects to copy style do not match')
+
+    to_copy, to_deepcopy = [], []
+
+    if type(new_element) == Run:
+        to_copy = ['bold', 'italic', 'underline']
+        to_deepcopy = ['font']
+
+    elif type(new_element) == Font:
+        to_copy = ['all_caps', 'bold', 'cs_bold', 'cs_italic',
+                   'double_strike', 'emboss', 'hidden', 'highlight_color',
+                   'imprint', 'italic', 'name', 'outline',
+                   'rtl', 'shadow', 'size', 'small_caps', 'snap_to_grid', 'spec_vanish',
+                   'strike', 'subscript', 'superscript', 'underline', 'web_hidden'
+                   ]
+        to_deepcopy = ['color']
+
+    elif type(new_element) == ColorFormat:
+        if original_element.type == MSO_COLOR_TYPE.THEME:
+            to_copy = ['theme_color']
+        elif original_element.type == MSO_COLOR_TYPE.RGB:
+            to_copy = ['rgb']
+
+    else:
+        return
+
+    for attr in to_copy:
+        if hasattr(original_element, attr):
+            setattr(new_element, attr, getattr(original_element, attr))
+
+    for attr in to_deepcopy:
+        if hasattr(new_element, attr) and hasattr(original_element, attr):
+            copy_style(getattr(new_element, attr),
+                       getattr(original_element, attr))
+
+
+def highlight(document, color):
     for paragraph in document.paragraphs:
-        for run in paragraph.runs:
+        runs = copy(paragraph.runs)
+        paragraph.clear()
+
+        for run in runs:
             original_text = run.text
             diminutives = find_diminutives(original_text)
-            run.clear()
 
             coursor = 0
             for start_position, end_position in diminutives:
                 # normal text
-                run.add_text(original_text[coursor:start_position])
+                new_run = paragraph.add_run(
+                    original_text[coursor:start_position], run.style)
+                copy_style(new_run, run)
+
                 # diminutive
-                run.add_text(original_text[start_position:end_position])
+                new_run = paragraph.add_run(
+                    original_text[start_position:end_position], run.style)
+                copy_style(new_run, run)
+                new_run.font.highlight_color = color
+
                 coursor = end_position
 
             # normal text
-            run.add_text(original_text[coursor:len(original_text)])
-            if diminutives:
-                run.font.highlight_color = WD_COLOR_INDEX.YELLOW
+            new_run = paragraph.add_run(
+                original_text[coursor:len(original_text)], run.style)
+            copy_style(new_run, run)
 
     return document
 
 
 def main():
+    colors = [attr for attr in dir(
+        WD_COLOR_INDEX) if attr.isupper() and not attr.startswith('_')]
+    default_color = 'RED' if 'RED' in colors else colors[-1]
+
     parser = argparse.ArgumentParser(description='Hightlight diminutives')
     parser.add_argument(
         '-i', '--input', help='Input docx file', required=True)
@@ -51,6 +109,8 @@ def main():
         '-o', '--output', help='Output docx file', required=True)
     parser.add_argument(
         '-f', '--force', help='Force output overwrite', action='store_true')
+    parser.add_argument('-c', '--color', help='Color to highlight diminutives',
+                        choices=colors, default=default_color)
     parser.add_argument("-v", "--verbose", help="debug output",
                         action="store_true")
 
@@ -60,23 +120,27 @@ def main():
     if args.verbose:
         L.setLevel('DEBUG')
 
+    if not hasattr(WD_COLOR_INDEX, args.color):
+        L.error('WD_COLOR_INDEX has no color `%s`', args.color)
+        return 1
+
     if not isfile(args.input):
         L.error('Not such file: %s', args.input)
-        return
+        return 1
 
     if not args.force and isfile(args.output):
         L.error('File exists: %s. Use -f/--force to overwrite.', args.output)
-        return
+        return 1
 
     try:
         document = Document(args.input)
     except PythonDocxError as e:
         L.error('Error when opening input file: %s', e)
-        return
+        return 1
 
-    highlighted_document = highlight(document)
+    highlighted_document = highlight(document, getattr(WD_COLOR_INDEX, args.color))
     highlighted_document.save(args.output)
 
 
 if __name__ == "__main__":
-    main()
+    exit(main())
